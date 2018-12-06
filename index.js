ef0fc74b9fd0a1e76b8cb7c1070bf50a67e67ed4
Brad Mortensen <b.mort86@gmail.com>
2018-12-06 10:39:23 -0700

    Initializing Project

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json())
+
+server.get('/', (req, res) =>{
+  res.status(200).json({ api: "up" })
+})
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => console.log(`\nrunning on port ${port}\n`));
---
+++ index.test.js
@@ -0,0 +1,10 @@
+const server = require('./index.js')
+
+describe('index.js', () => {
+  it('runs the test', () => {
+    expect(true).toBeTruthy();
+  });
+  it('runs the test', () => {
+    expect(true).toBeTruthy();
+  });
+});  
